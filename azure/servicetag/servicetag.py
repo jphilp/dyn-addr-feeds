97cf5b75400243fc9315de88165397cee4150412
Jason Philp <jphilp@pkivault.com>
2020-09-01 16:24:47 -0400

    Initial commit

---
+++ azure/servicetag/servicetag.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import json
+
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+
+locations = ["NorthCentralUS","SouthCentralUS" ]
+feedname = 'ServiceTags_Public_20200810.json'
+feed_server = 'AZURE'
+feed_url = 'jphilp.github.io'
+feed_update_interval = 1800
+feed_hold_interval = 864000
+feed_names = []
+feed_addresses = []
+
+
+with open(os.path.join(sys.path[0], feedname), "r") as read_file:
+    feed = json.load(read_file)
+
+
+for servicetag in feed['values'] :
+    if any((loc in servicetag['name'] or '.' not in servicetag['name']) for loc in locations):
+        service = servicetag['name'].split(".")
+        if len( service ) == 1 : service.append( "Global" )
+        feed_names.append({ 'name' : service[1] + "-" + service[0]+"-ipv4", 'path' : service[1] + "/" + service[0] + "/ipv4", 'update-interval' : feed_update_interval, 'hold-interval' : feed_hold_interval })
+        feed_addresses.append({ 'name' : "DYN-" + feed_server + "-" + service[1] + "-" + service[0]+"-ipv4", 'profile' : { 'feed-name' : [ { 'name' : service[1] + "-" + service[0]+"-ipv4"} ] } })
+        os.makedirs(os.path.join(sys.path[0], service[1], service[0]), exist_ok=True)
+        with open(os.path.join(sys.path[0], service[1], service[0] + "\ipv4"), "w+") as f:
+            for ipaddress in servicetag['properties']['addressPrefixes'] :
+                f.writelines(ipaddress + "\n")
+
+config_candidate = { 'configuration' : { 'security' : { 'dynamic-address' : { 'feed-server' : [ { 'name' : feed_server, 'url' : feed_url, 'feed-name' : feed_names } ], 'address-name': feed_addresses } } } }
+
+print(json.dumps(config_candidate, indent=4))
+
+dev = Device('172.16.100.222', user='jphilp', password='Juniper123').open()
+with Config(dev, mode='exclusive') as cu:  
+    cu.load(json.dumps(config_candidate), format='json')
+    cu.pdiff()
+    cu.commit()
+dev.close()
